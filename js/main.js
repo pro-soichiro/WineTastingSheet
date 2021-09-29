42128bd207450fc6eafdd0b33619b2e1e3e76bfa
pro-soichiro <dr.1987626@gmail.com>
2021-09-29 22:24:32 +0900

    [Update] 修正

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict';
 {
   const labels = document.querySelectorAll('label');
+  const btn = document.getElementById('btn');
+  const l = document.getElementsByClassName('line-through');
+  // getElementByClassNameで取得する値は配列ではないので配列に変換する必要がある
+  const lines = Array.prototype.slice.call(l);
 
   labels.forEach(label => {
     label.addEventListener('mouseover', () => {
@@ -10,4 +14,20 @@
       label.parentNode.classList.remove('appear');
     });
   });
+
+
+  lines.forEach(line => {
+    btn.addEventListener('click', () => {
+      line.classList.toggle('line-through-on');
+      const input = line.childNodes;
+      if(line.classList.contains('line-through-on') == true ){
+        btn.innerHTML = 'アシスト機能OFF';
+      }else{
+        btn.innerHTML = 'アシスト機能ON';
+      }
+    });
+  });
+
+
+
 }
