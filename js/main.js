5ced68f7d9be9d840977345a8d0c60822986fb12
pro-soichiro <dr.1987626@gmail.com>
2021-09-29 19:42:00 +0900

    [Update] hover時に画像を表示するJavaScriptVersion

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 'use strict';
-
 {
+  const labels = document.querySelectorAll('label');
 
-
+  labels.forEach(label => {
+    label.addEventListener('mouseover', () => {
+      label.parentNode.classList.add('appear');
+    });
+    label.addEventListener('mouseout', () => {
+      label.parentNode.classList.remove('appear');
+    });
+  });
 }
